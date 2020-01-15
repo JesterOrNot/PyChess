3a61bb13c85d334731edf9798df77a82935d1552
Sean Hellum <seanhellum45@gmail.com>
2020-01-15 17:52:30 +0000

    update

--- app.py
+++ app.py
@@ -1,49 +1,24 @@
 import pygame
- 
-# Define some colors
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
-# This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 20
-HEIGHT = 20
- 
-# This sets the margin between each cell
+WIDTH = 100
+HEIGHT = 100
 MARGIN = 5
- 
-# Create a 2 dimensional array. A two dimensional
-# array is simply a list of lists.
-grid = []
-for row in range(10):
-    # Add an empty array that will hold each cell
-    # in this row
-    grid.append([])
-    for column in range(10):
-        grid[row].append(0)  # Append a cell
- 
-# Set row 1, cell 5 to one. (Remember rows and
-# column numbers start at zero.)
-grid[1][5] = 1
- 
-# Initialize pygame
+grid = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
 pygame.init()
- 
 # Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
+WINDOW_SIZE = [325, 325]
 screen = pygame.display.set_mode(WINDOW_SIZE)
- 
-# Set title of screen
 pygame.display.set_caption("Array Backed Grid")
- 
-# Loop until the user clicks the close button.
 done = False
- 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
@@ -54,16 +29,12 @@ while not done:
             # Change the x/y screen coordinates to grid coordinates
             column = pos[0] // (WIDTH + MARGIN)
             row = pos[1] // (HEIGHT + MARGIN)
-            # Set that location to one
             grid[row][column] = 1
             print("Click ", pos, "Grid coordinates: ", row, column)
- 
     # Set the screen background
     screen.fill(BLACK)
- 
-    # Draw the grid
-    for row in range(10):
-        for column in range(10):
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
             color = WHITE
             if grid[row][column] == 1:
                 color = GREEN
@@ -73,13 +44,8 @@ while not done:
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
                               HEIGHT])
- 
     # Limit to 60 frames per second
     clock.tick(60)
- 
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
- 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
 pygame.quit()
